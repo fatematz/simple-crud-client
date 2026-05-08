6978cb1dd31e804ce3765fa389a41d4a33d453c1
Fatema <fatematz486@gmail.com>
2026-05-08 21:41:17 +0600

    Update getUserData.jsx

--- src/lib/getUserData.jsx
+++ src/lib/getUserData.jsx
@@ -2,12 +2,16 @@
 const getUserData = async() => {
   const res = await fetch('http://localhost:5000/users');
   const result = await res.json();
+  // console.log("result", result)
+  return result;
 
-  return (result);
 };
 
 export default getUserData;
 
 export const getUserById = async(userId) => {
-  
+  const res = await fetch(`http://localhost:5000/users/${userId}`)
+  const data = await res.json();
+
+  return data;
 }
