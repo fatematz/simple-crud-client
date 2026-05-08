9c4e79f9d05d4dda430a4c0da281cc76d23d8f10
Fatema <fatematz486@gmail.com>
2026-05-09 02:11:31 +0600

    Update page.jsx

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,13 +1,22 @@
 import getUserData from "@/lib/getUserData";
 import UserTable from "../components/userTable";
+import deleteUser, { createUser } from "@/lib/actions";
+import AddUsersModal from "../components/AddUsersModal";
 
 const UsersPage =  async () => { 
         const users = await getUserData()
         console.log("get Data",users);
        
-            return <div>
-            <h2>User Management </h2>
-            <UserTable users={users}></UserTable>
+            return <div className="">
+                <div className="flex justify-between items-center">
+            <h2>User Management {users.length} </h2>
+            <AddUsersModal createUserAction = {createUser} >
+
+            </AddUsersModal>
+            </div>
+                <div className="">
+            <UserTable users={users} deleteUserAction = {deleteUser} ></UserTable>
+        </div>
         </div>
 
 };
